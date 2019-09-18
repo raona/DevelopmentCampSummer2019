43f3888166e509e26614e6ae03bdd5c741023288
Albert <albert@MacBook-Pro-de-Albert.local>
2019-09-18 10:36:03 +0200

    finished development

---
+++ 3. Desarrollo web/build/precache-manifest.c0e41b9679de5aee4eb7b156bc2160cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2711eeeb7ac749a244b483eaeed1e930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e64b3d02e9723c68b3a",
+    "url": "/static/css/2.b9146f23.chunk.css"
+  },
+  {
+    "revision": "2e64b3d02e9723c68b3a",
+    "url": "/static/js/2.ec340346.chunk.js"
+  },
+  {
+    "revision": "46a0304358684dbb8b04",
+    "url": "/static/js/3.09b47ff8.chunk.js"
+  },
+  {
+    "revision": "374573e530fd52699e2d",
+    "url": "/static/js/4.6ff9355c.chunk.js"
+  },
+  {
+    "revision": "f547b05b4d7e40cc9b06",
+    "url": "/static/js/main.ac480a4b.chunk.js"
+  },
+  {
+    "revision": "393da99cb03698d51453",
+    "url": "/static/js/runtime~main.c648d1e8.js"
+  }
+]);
